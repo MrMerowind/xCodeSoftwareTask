da6eb9119e96b1d31d57fcb229cc927423b94888
MrMerowind <mrmerowind@gmail.com>
2022-12-15 18:28:18 +0100

    Add partial form.

--- sortingFrontend/src/app/app.component.ts
+++ sortingFrontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sortingFrontend';
+
+  sortNumbersForm: FormGroup = new FormGroup(
+    {
+      numbersInput: new FormControl("", Validators.compose([Validators.required, Validators.pattern("/^[0-9\,\s]$/")])),
+      isAscending: new FormControl(false)
+    }
+  );
 }
