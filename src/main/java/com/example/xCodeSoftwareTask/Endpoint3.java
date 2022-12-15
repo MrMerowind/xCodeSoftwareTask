d748fde4a70590302dd7e3a3670e714d1ef31cf9
MrMerowind <mrmerowind@gmail.com>
2022-12-15 16:25:58 +0100

    Add Endpoint3 response entity.

--- src/main/java/com/example/xCodeSoftwareTask/Endpoint3.java
+++ src/main/java/com/example/xCodeSoftwareTask/Endpoint3.java
@@ -1,5 +1,7 @@
 package com.example.xCodeSoftwareTask;
 import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
 public class Endpoint3 {
     
     @PostMapping("get-current-currency-value-command")
-    public CurrencyResponse  sort(@RequestBody Currency currencyInput) throws IOException
+    public ResponseEntity<CurrencyResponse> sort(@RequestBody Currency currencyInput) throws IOException
     {
         String url = "http://api.nbp.pl/api/exchangerates/rates/A/" + currencyInput.getCurrency() + "/today?format=json";
         RestTemplate restTemplate = new RestTemplate();
@@ -25,7 +27,7 @@ public class Endpoint3 {
         catch(Exception e){nbpr = "0.0";}
         
         CurrencyResponse cr = new CurrencyResponse(Double.parseDouble(nbpr));
-        return cr;    
+        return ResponseEntity.ok(cr);    
     }
 }
 
