a72f01cc7e06a8b5886ab2726a40898a3ccaa7b8
MrMerowind <mrmerowind@gmail.com>
2022-12-15 07:22:34 +0100

    Add endpoint1.

---
+++ src/main/java/com/example/xCodeSoftwareTask/Endpoint1.java
@@ -0,0 +1,15 @@
+package com.example.xCodeSoftwareTask;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "status/ping")
+public class Endpoint1 {
+    @GetMapping
+    public String ping()
+    {
+        return "pong";
+    }    
+}
--- src/test/java/com/example/xCodeSoftwareTask/XCodeSoftwareTaskApplicationTests.java
+++ src/test/java/com/example/xCodeSoftwareTask/XCodeSoftwareTaskApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.xCodeSoftwareTask;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class XCodeSoftwareTaskApplicationTests {
 
+	private Endpoint1 controller = new Endpoint1();
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		
+		Assert.notNull(controller, "Endpoint1 controller must be defined.");
 	}
 
 }
