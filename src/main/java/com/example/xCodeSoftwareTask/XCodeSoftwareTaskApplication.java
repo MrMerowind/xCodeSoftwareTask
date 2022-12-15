005085a121258a74c0a98c2aede0f730c69e792e
MrMerowind <mrmerowind@gmail.com>
2022-12-15 06:52:49 +0100

    Project init.

---
+++ src/main/java/com/example/xCodeSoftwareTask/XCodeSoftwareTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.xCodeSoftwareTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XCodeSoftwareTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XCodeSoftwareTaskApplication.class, args);
+	}
+
+}
